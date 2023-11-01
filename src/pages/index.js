4a64a42bdeb0c4856661c13ec9755c1e4f0017d2
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-11-01 20:51:45 +0800

    chore: change home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,9 @@ export default function Home() {
           <h1 className={styles.homePage1Header}>
             The Stanford Prison Experiment
           </h1>
-          <h2 className={styles.homePage1SubHeader}>or Zimbardo et. al. 1971</h2>
+          <h2 className={styles.homePage1SubHeader}>
+            or Zimbardo et. al. 1971
+          </h2>
         </div>
         <div className={styles.homePage2}>
           <h1 className={styles.homePage2Header}>
@@ -28,16 +30,18 @@ export default function Home() {
             The Stanford Prison Experiment was an experiment set up by Philip
             Zimbardo of Stanford. He wanted to see how stereotypes of roles such
             as prisoners and guards could influence the behaviour of people in
-            those social roles. He did this by setting up an experiment where 20
-            male college students who signed up from a newspaper for $15/hour
-            were assigned to 10 prisoners and 10 guards. They were given proper
-            prison treatment, and they had rules in place. These rules, however,
-            were broken, causing extreme psychological damage to some prisoners
-            and ended after 6 days, instead of the planned 14 days. The Stanford
-            Prison Experiment created a huge debate on ethics for psychological
-            experiments and led to the creation of multiple committees that were
-            in charge of regulating experiments to ensure that they were ethical
-            and did not cause harm to people.
+            those social roles. He also wanted to observe the effects of
+            de-individuation on prisoners and guards. He did this by setting up
+            an experiment where 20 male college students who signed up from a
+            newspaper for $15/hour were assigned to 10 prisoners and 10 guards.
+            They were given proper prison treatment, and they had rules in
+            place. These rules, however, were broken, causing extreme
+            psychological damage to some prisoners and ended after 6 days,
+            instead of the planned 14 days. The Stanford Prison Experiment
+            created a huge debate on ethics for psychological experiments and
+            led to the creation of multiple committees that were in charge of
+            regulating experiments to ensure that they were ethical and did not
+            cause harm to people.
           </p>
           <h1 className={styles.homePage2Header}>
             What psychological effects were caused by the experiment?
@@ -65,13 +69,23 @@ export default function Home() {
                 <li>People's imagination,</li>
               </ul>
             </ul>
+            These things caused effects such as:
+            <ol>
+              <li>Prisoners having depression and anxiety</li>
+              <li>Deindividuation</li>
+              <li>Prisoners forgetting their names</li>
+            </ol>
+            and other mild psychological effects.
           </p>
           <h1 className={styles.homePage2Header}>Who was Philip Zimbardo?</h1>
           <p className={styles.homePage2Description}>
             Philip Zimbardo (1933-) was an American psychologist and professor
             who worked at Stanford University. He became well-known for the
             experiment he set up - Zimbardo et. al., which later came about to
-            be the Stanford Prison Experiment. His experiment raised lots of ethical concerns regard
+            be the Stanford Prison Experiment. His experiment raised lots of
+            ethical concerns regarding experiments, and caused a lot of debate
+            on ethics and the creation of committees which has transformed the
+            way that modern psychological ethics works.
           </p>
         </div>
       </main>
